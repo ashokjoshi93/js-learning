f6c971bd229d5cbc9f3dbfac3b6abe66e8988b51
Ashok Joshi <joshiashok48684868@gmail.com>
2017-02-27 18:28:16 +0530

    edited

--- maxmin.js
+++ maxmin.js
@@ -1,6 +1,9 @@
-var seq=[4,5,9,7,2,3,6];
 
-seq.sort();
+function maxmin(ab)
+{
+  ab.sort();
+  console.log("2nd Min is:"+ab[1]);
+  console.log("2nd Max is:"+ab[ab.length-2]);
+}
 
-console.log("2nd Min is:"+seq[1]);
-console.log("2nd Max is:"+seq[seq.length-2]);
+maxmin([8,9,6,7,5]);
