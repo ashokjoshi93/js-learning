c39f38cb04a9da5903068d7702f04a91904f04e9
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-02-27 15:34:48 +0530

    Update strseq.js

--- strseq.js
+++ strseq.js
@@ -1,20 +1,3 @@
-
-var str = "azxriop";
-var arr = new Array();
-arr = str.split("");
-console.log(arr);
-
-for(var i=0;i<arr.length;i++)
-{
-  for(var j=i;j<arr.length;j++)
-  {
-    if(arr[i]>arr[j])
-    {
-       var temp=arr[i];
-      arr[i]=arr[j];
-      arr[j]=temp;
-    }
-  }
-}
-
-console.log(arr);
+var str = "Ridham";
+var sorted = str.split('').sort().join('');
+console.log(sorted);
